271a1a07c9b7a8b76cbf4fe1b4163408141f9601
Jackeagle <jackeagle102@gmail.com>
2019-06-26 15:14:56 +0200

    Z01R: Blissify Asus Zenfone 5z
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I3f75935ed971c6d2620edacf7c49f516891cfbf2

--- bliss_Z01R.mk
+++ bliss_Z01R.mk
@@ -22,14 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Inherit from Z01R device
 $(call inherit-product, device/asus/Z01R/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := Z01R
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := Zenfone 5Z
-PRODUCT_NAME := lineage_Z01R
+PRODUCT_NAME := bliss_Z01R
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
