3faa6092b8df945930a68bd5095be7287515469c
PythonLimited <Leonardomeitz@gmail.com>
2020-06-28 15:09:19 +0530

    Z01R: Migrate to livedisplay 2.0
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Co-authored-by: Bruno Martins
    Co-authored-by: PythonLimited <Leonardomeitz@gmail.com>

--- device.mk
+++ device.mk
@@ -136,6 +136,10 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_Z01R
+	
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
