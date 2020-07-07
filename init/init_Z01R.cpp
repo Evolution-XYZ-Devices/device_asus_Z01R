0e2ec43aa9ef6df8caf5717bf546a75d7308ee0d
Joey Huab <joey@evolution-x.org>
2020-07-08 00:05:08 +0530

    Z01R: Update to Coral's July fingerprint

--- init/init_Z01R.cpp
+++ init/init_Z01R.cpp
@@ -56,7 +56,7 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys");
     property_override("ro.product.device", "ASUS_Z01R_1");
     property_override("ro.product.model", "Zenfone 5Z");
     property_override("ro.product.brand", "Asus");
