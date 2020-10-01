fbf85c29ecb576f74425f79093417d7b67c36ccb
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    Z01R: Remove packages at build time
    Nuke Google Recorder, NgaResources and ARCore
    
    - Save some system space

--- device.mk
+++ device.mk
@@ -371,6 +371,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
